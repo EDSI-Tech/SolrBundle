43d9d4d7238119bc6777ea2cb7bfbd5f373ad0c2
Kevin Robatel <k.robatel@edsi-tech.com>
2015-03-09 11:00:37 +0100

    Not override user defined type for method annotation

--- Doctrine/Annotation/AnnotationReader.php
+++ Doctrine/Annotation/AnnotationReader.php
@@ -72,7 +72,10 @@ class AnnotationReader
             $method->setAccessible(true);
             $annotation->value = $method->invoke($entity);
             $annotation->name = lcfirst(preg_replace('/get/', '', $method->getName(), 1));
-            $annotation->type = 'read_only';
+
+            if ($annotation->type != null) {
+                $annotation->type = 'read_only';
+            }
 
             $fields[] = $annotation;
         }
